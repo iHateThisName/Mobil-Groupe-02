4c938decdf2ca2753ac458c3fa1c802143ac3711
Ivar Bredeli <ivabr@stud.ntnu.no>
2022-10-11 18:23:07 +0200

    Created a authentication dart file to handle authentication.

---
+++ mobileapp_project/lib/services/authentication.dart
@@ -0,0 +1,52 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class AuthBase {
+
+  Stream<User?> authStateChanges();   //for StreamBuilder
+  Future<User?> signInAnonymously();
+  Future<void> signOut();
+  Future<User?> signInWithEmailAndPassword(String email, String password);
+  Future<User?> createUserWithEmailAndPassword(String email, String password);
+
+
+}
+
+class Auth implements AuthBase {
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Stream<User?> authStateChanges() => _firebaseAuth.authStateChanges();
+
+  @override
+  Future<User?> signInAnonymously() async {
+    final userCredential = await _firebaseAuth.signInAnonymously();
+    return userCredential.user;
+  }
+
+  @override
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+
+  //Email
+
+  @override
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    final userCredential = await _firebaseAuth.signInWithCredential(
+      EmailAuthProvider.credential(email: email, password: password),
+    );
+    return userCredential.user;
+  }
+
+  @override
+  Future<User?> createUserWithEmailAndPassword(
+      String email, String password) async {
+    final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return userCredential.user;
+  }
+}
