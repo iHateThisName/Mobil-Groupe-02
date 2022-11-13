b35a0409e4f1048bed29b170c9ffbe8ed93c95f7
vegard-ok <vegard_overaas@hotmail.com>
2022-11-13 20:48:59 +0100

    Imported a toilet marker, finished unit testing the profile class

---
+++ mobileapp_project/test/unit_tests/profile_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobileapp_project/app/models/profile_model.dart';
+
+void main() {
+  test('Profile properties', () {
+    final profile = Profile(username: 'Vegard', score: 50);
+    // Checks if the username and score matches the string and int
+    expect(profile.username, 'Vegard');
+    expect(profile.score, 50);
+  });
+}
