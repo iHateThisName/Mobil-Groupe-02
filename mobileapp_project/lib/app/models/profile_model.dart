e65070c9fcddc99a921f1e779a1f880abd8277fe
Ivar <ivabr@stud.ntnu.no>
2022-11-22 13:27:22 +0100

    Added email to profile_model

--- mobileapp_project/lib/app/models/profile_model.dart
+++ mobileapp_project/lib/app/models/profile_model.dart
@@ -1,16 +1,20 @@
+
 class Profile {
   final String username;
   final int score; // Give the user a point system.
+  final String email;
 
   Profile({
     required this.username,
     required this.score,
+    required this.email
   });
 
   Map<String, dynamic> toMap() {
     return {
       "username": username,
       "score": score,
+      "email": email,
     };
   }
 }
