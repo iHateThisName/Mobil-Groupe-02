515165e75a07cabed0fc162b03bd71b100a24c33
Vegard Karlstrøm <vegard_overaas@hotmail.com>
2022-12-03 03:46:58 +0100

    Merge branch 'CurrentLocationMarker' into dev

--- mobileapp_project/lib/app/pages/landing_page.dart
+++ mobileapp_project/lib/app/pages/landing_page.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mobileapp_project/app/pages/mappage.dart';
 import 'package:mobileapp_project/app/pages/map_page.dart';
+import 'package:mobileapp_project/app/pages/welcome.dart';
 import 'package:mobileapp_project/services/authentication.dart';
 import 'package:mobileapp_project/services/database.dart';
 import 'package:provider/provider.dart';
